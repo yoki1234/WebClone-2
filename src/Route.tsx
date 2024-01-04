29f364852fe0bbe9c88216ad15263912c8fc370e
akshay <akshay.ashok@vonnue.com>
2024-01-04 22:35:02 +0530

    space-x: nav-bar-button

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SpaceXIndex from "./space-x/SpaceXIndex";
 import HomePage from "./HomePage";
-import { navbarData } from "./space-x/src/data";
-import Navbar from "./space-x/src/container/Navbar";
+// import { navbarData } from "./space-x/src/data";
+// import Navbar from "./space-x/src/container/Navbar";
+// import BannerButton from "./space-x/src/components/BannerButton";
+import CTAbanner from "./space-x/src/container/CTAbanner";
 
-function App() {
+function RoutePage() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/space-x" element={<SpaceXIndex />} />
+        <Route path="/test" element={<CTAbanner />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
+export default RoutePage;
